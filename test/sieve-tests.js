45458793be1083b116450ff029a3535bb92bbe75
Phil Ananin <phil.ananin@gmail.com>
2016-04-21 15:01:56 +0100

    Add failing test for sieve(1)

--- test/sieve-tests.js
+++ test/sieve-tests.js
@@ -2,9 +2,14 @@ const chai = require('chai');
 const expect = chai.expect;
 const Sieve = require('../src/sieve.js');
 
-describe('Sieve', function() {
-  it('sieve(n) should return an empty array when a negative number is passed in', function() {
+describe('Sieve', () => {
+  it('sieve(n) should return an empty array when a negative number is passed in', () => {
     const sieve = new Sieve();
     expect(sieve.sieve(-2)).to.be.empty;
+  });
+
+  it('sieve(1) should return the first prime number in an array (2)', () => {
+    const sieve = new Sieve();
+    expect(sieve.sieve(1)).to.eql([2]);
   });
 });
