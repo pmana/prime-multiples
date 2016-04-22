73b7883a56d7e3918e131421860bf2b2c8de3e39
Phil Ananin <phil.ananin@gmail.com>
2016-04-22 14:33:59 +0100

    Add test for header row

--- test/multiplication-table-tests.js
+++ test/multiplication-table-tests.js
@@ -7,4 +7,8 @@ describe('MultiplicationTable', () => {
   it('should return an empty array when empty array is passed in', () => {
     expect(table.multiply([])).to.be.empty;
   });
+
+  it('should include a header row', () => {
+    expect(table.multiply([1])[0]).to.eql([,1]);
+  });
 });
